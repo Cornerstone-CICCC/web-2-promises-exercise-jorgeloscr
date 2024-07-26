6dc652cef813fddf969f097ff96ec65ab65b47e2
Jorge Ortega <jorgeloscr@hotmail.com>
2024-07-26 00:20:54 -0700

    Completed

--- file_reader.js
+++ file_reader.js
@@ -1,10 +1,43 @@
 const fs = require("fs").promises;
 
-// THEN-CATCH SOLUTION BELOW THIS LINE
 
+const readFile = (filename) => fs.readFile(filename, 'utf8');
 
+// THEN-CATCH SOLUTION BELOW THIS LINE 
 
-
-
+const readFilesThenCatch = () => {
+    Promise.all([
+        readFile('firstname.txt'),
+        readFile('lastname.txt'),
+        readFile('age.txt'),
+        readFile('hobbies.txt')
+    ])
+    .then(([firstname, lastname, age, hobbies]) => {
+        console.log(`${firstname.trim()} ${lastname.trim()} is ${age.trim()} years old and his hobbies are ${hobbies.trim()}`);
+    })
+    .catch(err => {
+        console.error('Error reading files:', err);
+    });
+};
 
 // ASYNC/AWAIT SOLUTION BELOW THIS LINE
+const readFilesAsyncAwait = async () => {
+    try {
+        const [firstname, lastname, age, hobbies] = await Promise.all([
+            readFile('firstname.txt'),
+            readFile('lastname.txt'),
+            readFile('age.txt'),
+            readFile('hobbies.txt')
+        ]);
+        console.log(`${firstname.trim()} ${lastname.trim()} is ${age.trim()} years old and his hobbies are ${hobbies.trim()}`);
+    } catch (err) {
+        console.error('Error reading files:', err);
+    }
+};
+
+
+readFilesThenCatch();
+readFilesAsyncAwait();
+
+
+
